14cb14c93a5c09255ae88cf48ea6c60d6328986a
Bhavana Shrotri <Bhavana.Shrotri@Klingelnberg.com>
2023-01-19 11:53:58 +0530

    creating events

--- EventStoreDemo/Program.cs
+++ EventStoreDemo/Program.cs
@@ -1,28 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EventStore.Client;
-using EventStore.ClientAPI;
 using EventStoreDemo;
 using Newtonsoft.Json;
-using System.Net;
 using System.Text;
 
 static string StreamId(Guid id)
 {
     return $"BanckAccount{id}";
 }
-var address = IPAddress.Parse("127.0.0.1");
-
-//var settings = ConnectionSettings
-//    .Create()
-//    .DisableServerCertificateValidation()
-//    .DisableTls()
-//    .KeepReconnecting()
-//    .Build();
-
-//IEventStoreConnection connection = EventStoreConnection.Create(
-//        new IPEndPoint(IPAddress.Loopback, 1113));
-
-//await connection.ConnectAsync();
 
 //GCRP
 
@@ -31,45 +16,28 @@ var settings = EventStoreClientSettings
 
 var client = new EventStoreClient(settings);
 
-var evt = new
+var aggregateId = Guid.NewGuid();
+
+List<IEvent> eventsToRun = new List<IEvent>
 {
-    EntityId = Guid.NewGuid().ToString("N"),
-    mportantData = "I wrote my first event!"
+    new AccountCreatedEvent(aggregateId, "Bhavana Shrotri"),
+    new FundsDepositedEvent(aggregateId, 100000),
+    new FundsWithdrawedEvent(aggregateId, 50000)
 };
-var jsonString = JsonConvert.SerializeObject(evt);
 
-var eventData = new EventStore.Client.EventData(
-    Uuid.NewUuid(),
-    "TestEvent",
-     Encoding.UTF8.GetBytes(jsonString)
-);
-
-await client.AppendToStreamAsync(
-    "some-stream",
-    StreamRevision.None,
-     new[] { eventData });
+foreach (var item in eventsToRun)
+{
+    var jsonString = JsonConvert.SerializeObject(item);
+    var jsonPayload = Encoding.UTF8.GetBytes(jsonString);
+    var eventStoreDataType = new EventData(Uuid.NewUuid(), item.GetType().Name, jsonPayload, null);
+    await client.AppendToStreamAsync(StreamId(aggregateId), StreamState.Any, new[] { eventStoreDataType });
+}
 
 var result = client.ReadStreamAsync(
     Direction.Forwards,
     "some-stream",
     EventStore.ClientAPI.StreamPosition.Start);
 
-
-//var aggregateId = Guid.NewGuid();
-
-//List<IEvent> eventsToRun = new List<IEvent>
-//{
-//    new AccountCreatedEvent(aggregateId, "Bhavana Shrotri"),
-//    new FundsDepositedEvent(aggregateId, 100000),
-//    new FundsWithdrawedEvent(aggregateId, 50000)
-//};
-
-//foreach (var item in eventsToRun)
-//{
-//    var jsonString = JsonConvert.SerializeObject(item);
-//    var jsonPayload = Encoding.UTF8.GetBytes(jsonString);
-//    var eventStoreDataType = new EventStore.ClientAPI.EventData(Guid.NewGuid(), item.GetType().Name, true, jsonPayload, null);
-//    await connection.AppendToStreamAsync(StreamId(aggregateId), ExpectedVersion.Any, eventStoreDataType);
-//}
+Console.WriteLine(result);
 
 Console.ReadLine();
